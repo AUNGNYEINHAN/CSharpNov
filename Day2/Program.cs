60b5e98000cf84d15e0804495f6f3e706afd2304
fukaeri404 <aungnyeinhan00@gmail.com>
2024-11-30 19:19:53 +0900

    「追加」Day2 and Day2HW

---
+++ Day2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            //Console.WriteLine("Enter your nationality :");
+            //String nat = Console.ReadLine().ToLower();
+            //Console.WriteLine("Enter your age ;");
+            ////int age = int.Parse(Console.ReadLine());
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //if (nat == "mm" && age >= 18)
+            //{
+            //    Console.WriteLine("Can Vote");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Cant Vote");
+            //}
+
+            int marks = 65;
+
+            if (marks >= 50)
+            {
+                if (marks >= 85)
+                {
+                    Console.WriteLine("Pass with D");
+                }
+                else if(marks >= 65){
+                    Console.WriteLine("Pass with Credit");
+                }
+                else
+                {
+                    Console.WriteLine("Pass");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Stupid");
+            }
+
+            int month = 2, year = 2024;
+
+            if (month == 2)
+            {
+                if (year % 4 == 0)
+                {
+                    Console.WriteLine("29 days");
+                }
+                else
+                    Console.WriteLine("28 days");
+            }
+            else if (month == 4 || month == 6 || month == 9 || month ==11)
+            {
+                Console.WriteLine("30 days");
+            }
+            else
+            {
+                Console.WriteLine("31 days");
+            }
+
+
+            switch (month)
+            {
+                case 2:
+                    Console.WriteLine("28 days");break;
+                case 4: case 6: case 9: case 11:
+                    Console.WriteLine("30 days"); break;
+                default:
+                    Console.WriteLine("31 days");break;
+            }
+        }
+    }
+}
---
+++ Day2HW/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//A  +  A    -> A, O
+//A  +  B	   -> A, B, AB, O
+//A  +  AB   -> A, B, AB
+//A  +  O	   -> A, O
+//B  +  B	   -> B, O
+//B  +  AB   -> A, B, AB
+//B  +  O	   -> B, O
+//AB +  AB  -> A, B, AB
+//AB +  O   -> A, B
+//O  +  O	   -> O
+
+namespace Day2HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type your father's blood type : ");
+            String fatherBType = Console.ReadLine().ToUpper();
+            Console.WriteLine("Type your mother's blood type : ");
+            String motherBType = Console.ReadLine().ToUpper();
+            Console.WriteLine("Your Child's possible Blood Groups are : ");
+
+
+
+
+            
+
+            if (fatherBType == "AB" || motherBType == "AB")
+            {
+                Console.WriteLine("A");
+                Console.WriteLine("B");
+
+                if (fatherBType != "O" && motherBType != "O")
+                {
+                    Console.WriteLine("AB");
+                }
+
+            }
+            else
+            {
+               
+                //must print if both of them not AB
+                Console.WriteLine(fatherBType);
+
+                //no need to print again if both same
+                if(fatherBType != motherBType)
+                {
+                    Console.WriteLine(motherBType);
+                    if (fatherBType != "O" && motherBType != "O")
+                    {
+                        Console.WriteLine("O");
+                        Console.WriteLine("AB");
+                    }
+                    
+
+                }
+                else
+                {
+                    //no need to print again if fat.. is O already
+                    if (fatherBType != "O")
+                    {
+                        Console.WriteLine("O");
+                    }
+                }
+
+              
+
+
+            }
+
+        }
+    }
+}
