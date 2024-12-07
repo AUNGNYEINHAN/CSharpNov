8be467b1cb2b2538d4d237d33fc374e216ed2640
fukaeri404 <aungnyeinhan00@gmail.com>
2024-12-07 15:30:49 +0900

    added Day5 and Day5 HW

---
+++ Day5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace Day5
+{
+    internal class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            numbers[1] = 1;
+            numbers[2] = 5;
+
+
+            int n = 0;
+        ITC: if (n < 5)
+            {
+                Console.WriteLine(numbers[n]);
+                n++;
+                goto ITC;
+            }
+
+
+            ArrayList mydata = new ArrayList();
+            mydata.Add(10);
+            mydata.Add("sdafds");
+            mydata.Add(10.34);
+            mydata.Add(DateTime.Now);
+            foreach (var res in mydata)
+            {
+                Console.WriteLine(res);
+            }
+
+            int a = 10, b = 20, c = 30;
+
+            int*[] pointerArray = new int*[3];
+
+            pointerArray[0] = (int*)a;
+            pointerArray[1] = (int*)b;
+            pointerArray[2] = (int*)c;
+
+            foreach (var t in pointerArray)
+            {
+                Console.WriteLine((int)t);
+            }
+
+
+        }
+    }
+}
---
+++ Day5HW/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day5HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number : ");
+
+            int userInput = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= userInput; i++)
+            {
+                for (int j = 1; j <= 12; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " : " + i * j);
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
